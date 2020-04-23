aa213cc54fc6f68e306d5c1d5be9f85c62c3d18d
Cronus023 <anlrey.klochko@mail.ru>
2020-04-23 19:43:00 +0300

    update ChatMessage

--- src/entity/ChatMessage.java
+++ src/entity/ChatMessage.java
@@ -1,5 +1,34 @@
 package entity;
 
 public class ChatMessage {
-
+	//text message
+	private String message;
+	//author of message
+	private ChatUser author;
+	// time of message
+	private long timestamp;
+	public ChatMessage(String message, ChatUser author, long timestamp) {
+		super();
+		this.message = message;
+		this.author = author;
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public ChatUser getAuthor() {
+		return author;
+	}
+	public void setAuthor(ChatUser author) {
+		this.author = author;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
 }
